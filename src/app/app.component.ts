2f4d8a1ab0fa0d047b96985a0443ddedfcc06848
suhail_khan28 <suhail.khan@walletdoc.com>
2021-10-23 15:26:07 +0530

    Typing test page created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typingGames';
+  randomText = lorem.sentence();
+  correctText = '';
+
+  onChangeInput(event: any) {
+    const typeValue = event.target.value;
+    this.correctText = typeValue;
+    console.log(this.correctText);
+  }
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'wrong';
+  }
 }
